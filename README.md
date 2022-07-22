8beb99186137d8e81b8ce145b4f483b7dfc4ce14
RominaGonella <rominagonfur@gmail.com>
2022-07-22 01:49:38 +0000

    versión final

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Template for Machine Learning projects
+# Resumen del proceso
+
+1. En el primer paso se importan las librerías y se cargan los datos desde la web. El dataset contiene 891 reviews de apps en Google Play, el objetivo es crear un modelo Naive Bayes para predecir si el review es positivo o negativo interpretando el texto.
+2. Se realiza un breve preprocesamiento quitando una variable que no se necesita, sacando espacios y pasando texto a minúscula.
+3. Se separan los datos en muestras de entrenamiento (train) y control o evaluación (test) estratificando la variable target.
+4. Se aplica la función *CounterVectorizer* para transformar la variable de texto a numérico, creando una columna para cada palabra distinta. Se eliminan las palabras que no aportan usando como referencia el idioma inglés.
+5. Se manipula el diccionario de palabras que devuelve la función, creando un data frame con la frecuencia de cada palabra. Se intentó indentificar qué palabra representa cada columna de X, pero no se logró. No lo pedía el ejercicio pero me pareción interesante explorarlo.
+6. Se ajusta un modelo NaiveBayes de tipo multinomial.
+7. Se prueba el modelo con algunos textos arbitrarios.
+8. Se guarda el modelo en la carpeta 'models'.
--- src/app.py
+++ src/app.py
@@ -1,4 +1,39 @@
-from utils import db_connect
-engine = db_connect()
+# primero ejecutar desde consola: pip install -r requirements.txt
 
-# your code here
+# librerías a utilizar
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+
+# datos
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/naive-bayes-project-tutorial/main/playstore_reviews_dataset.csv')
+
+# elimino la primera variable
+data = data.drop(columns = 'package_name')
+
+# elimino espacios al inicio y al final, paso caracteres a minúscula
+data['review'] = data['review'].str.strip().str.lower()
+
+# separo en X e y
+X = data['review']
+y = data['polarity']
+
+# separo en train y test, eligiendo proporcionalmente valores de y = 0 o 1 de acuerdo al dataset completo
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = y, test_size = 0.25, random_state = 42)
+
+# función para vectoriza la variable reviews (X)
+vec = CountVectorizer(stop_words = 'english')
+
+# se aplica la vectorización a X_train y X_test
+X_train = vec.fit_transform(X_train).toarray()
+X_test = vec.transform(X_test).toarray()
+
+# ajusto modelo Naive Bayes
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# se guarda el modelo
+filename = '../models/nb_model.sav'
+pickle.dump(model, open(filename,'wb'))
